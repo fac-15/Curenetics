4a04ac99d831d356ffceeaa0a2612733e38ca997
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-02-12 15:43:22 +0000

    results card icons

---
+++ src/Components/SmallCard/SmallCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./small-card.css";
+// import { Link } from "react-router-dom";
+
+const SmallCard = props => {
+  const back = require("./arrow-backward.svg");
+  const forward = require("./arrow-forward.svg");
+  const tick = require("./tick.svg");
+  const calendar = require("./calendar.svg");
+  const marker = require("./marker.svg");
+  const file = require("./summary-file.svg");
+
+  //   console.log(props);
+  //   const title = props.appName;
+
+  return <article className="card-small">{back}</article>;
+};
+
+export default SmallCard;
