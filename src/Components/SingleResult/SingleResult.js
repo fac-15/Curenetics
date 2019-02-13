2da6a1551d47dcbadd8e3d05d80ab3c90522377a
susanX <susan.gooding@yahoo.co.uk>
2019-02-13 12:19:11 +0000

    singlepage

--- src/Components/SingleResult/SingleResult.js
+++ src/Components/SingleResult/SingleResult.js
@@ -4,24 +4,24 @@ import "./single-result.css";
 import "../Results/Results";
 import { Results } from "../Results/Results";
 
-export class SingleResult extends React.Component {
-  render() {
-    const text = "Single Result here";
-    console.log(this.props);
-    return (
-      <div className="small-card">
-        <p>In a new Component!</p>
-        <p>{text}</p>
-        <div>{this.props.item}</div>
-      </div>
-    );
-  }
-}
+// export class SingleResult extends React.Component {
+//   render() {
+//     const text = "Single Result here";
+//     console.log(this.props);
+//     return (
+//       <div className="small-card">
+//         <p>In a new Component!</p>
+//         <p>{text}</p>
+//         <div>{this.props.item}</div>
+//       </div>
+//     );
+//   }
+// }
 
 function single(Component){
   render() {
     const text = "Single Result here";
-    console.log(this.props);
+    console.log(props);
     return (
       <div className="small-card">
         <p>In a new Component!</p>
