5c8619e1b096cc4b8e6dc839508b8e9784167e21
Martin Bagshaw <martinbagshaw1@gmail.com>
2019-02-21 17:31:03 +0000

    recently viewed from localStorage

---
+++ api_keys/bing_key.js
@@ -0,0 +1,1 @@
+export const apiKey = "Ausvq83a6ukNg0_KjGutwIYLTSd4MnK7V_qQsir4Ma1_jeVKxTt-D8qK1t8LDLNV";
---
+++ src/Components/Filters/Viewed.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// viewed and bookmarked items
+// - when clicked, modify the list to show stuff in localStorage
+const Viewed = props => {
+  //   const titleArr = props.title;
+  return (
+    <div className="saved-trials">
+      <button className="filter-button" onClick={e => props.recentlyViewed()}>
+        <svg
+          aria-labelledby="recently-viewed"
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          viewBox="0 0 48 48"
+        >
+          <title id="recently-viewed" lang="en">
+            Recently Viewed
+          </title>
+          <path d="M12 5v10l9 9-9 9v10h24V33l-9-9 9-9V5H12zm20 29v5H16v-5l8-8 8 8zm-8-12l-8-8V9h16v5l-8 8z" />
+        </svg>
+        Recently Viewed
+      </button>
+    </div>
+  );
+};
+
+export default Viewed;
